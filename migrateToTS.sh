602e76b3f7939df4c8359d334902cb82b61b019a
Santiago <srt6221@gmail.com>
2020-05-23 14:03:18 -0500

    package y tsconfig

---
+++ migrateToTS.sh
@@ -0,0 +1,4 @@
+# Instalamos las dependencias de typescript para el body parser, express y node
+npm install --save-dev @types/body-parser @types/express ts-node nodemon typescript
+# Inicializamos la configuración de typescript en el proyecto
+npx tsc -init
